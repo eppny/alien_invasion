31766ad327c2d7330a9df9423c75be407071873c
mikej <mike92rj@gmail.com>
2021-02-13 09:52:41 +0000

    Move to src folder, add settings file

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -2,6 +2,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
     
@@ -9,7 +11,12 @@ class AlienInvasion:
         """Initialize the game and create game resources"""
         pygame.init()
         
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        
+        # TODO
+        # bg = pygame.images.load("images\background.jpg")
+        
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         
     def run_game(self):
@@ -23,6 +30,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     running = False		
                     
+            # Redraw the screen
+            self.screen.fill(self.settings.bg_color)
+            
             # Make the most recently drawn screen visible
             pygame.display.flip()
             
---
+++ src/settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 09:36:33 2021
+
+@author: Mike
+"""
+
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+    
+    def __init__(self):
+        
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
